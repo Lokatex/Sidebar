d50a67fb920b5b1c10999958031b9aec5ab4fd66
PanTester7 <80033919+Lokatex@users.noreply.github.com>
2025-02-25 14:12:58 +0100

    pocztek
    wprowadzony porzadek

---
+++ app-sidebar/src/App.jsx
@@ -0,0 +1,16 @@
+import logo from '../public/img/colorful-eid-mubarak-crescent-moon-greeting/13745.jpg';
+import './App.css';
+import Sidebar from './components/Sidebar';
+
+
+function App() {
+  
+
+  return (
+    <div>
+      <Sidebar />
+    </div>
+  );
+}
+
+export default App
---
+++ app-sidebar/src/components/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React, {useState} from "react";
+import logo from '../../public/img/colorful-eid-mubarak-crescent-moon-greeting/13745.jpg';
+
+export default function Sidebar() {
+
+    const [open,setOpen] = useState(true)
+
+    return(
+        <nav className=' shadow-md h-screen w-60 bg-blue-700'>
+
+        </nav>
+    )
+}
+
